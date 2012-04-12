8513e7191e1e20f95644ebf0d31daa6caaab708f
Aziz Light <aziz@azizlight.me>
2012-04-12 16:13:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileBaboon::Application.config.secret_token = '2a30db45b175319b7a08e853fd6b1683f19eb8422070d34b42acd156ea56dae6a0a60f551ec7b43bf7eff417a5df5894e4e882a637ea9b12609358f7b8c6c7a2'
