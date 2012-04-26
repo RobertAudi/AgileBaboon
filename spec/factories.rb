e4dd6090cfff890b55e6232b354ce91e5be46154
Aziz Light <aziz@azizlight.me>
2012-04-26 22:13:29 +0100

    Added a user factory for the User model

--- spec/factories.rb
+++ spec/factories.rb
@@ -12,4 +12,12 @@ FactoryGirl.define do
     sequence(:password)              { |n| "password" }
     sequence(:password_confirmation) { |n| "password" }
   end
+
+  factory :user do
+    sequence(:username)              { |n| "user#{n}" }
+    sequence(:email)                 { |n| "user#{n}@example.com" }
+    sequence(:password)              { |n| "password" }
+    sequence(:password_confirmation) { |n| "password" }
+    client_id 1
+  end
 end
