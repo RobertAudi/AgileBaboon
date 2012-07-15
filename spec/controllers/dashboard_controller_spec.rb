0fc1af3e8e999c4e56a33effc77f6959eff58602
Aziz Light <aziz@azizlight.me>
2012-07-15 15:32:16 +0100

    Fixed the dashboard controller spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -6,6 +6,8 @@ describe DashboardController do
 
   before(:each) do
     @request.host = "#{client.account_name}.lvh.me"
+    ActsAsTenant.current_tenant = Client.find_by_account_name(client.account_name)
+    controller.log_in(create(:user))
   end
 
   describe "GET 'index'" do
