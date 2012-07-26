f2584cafb0d4b90b3629db17158b20eeac490c51
Aziz Light <aziz@azizlight.me>
2012-07-26 15:46:02 +0100

    Make the roles table use the public db

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -1,6 +1,6 @@
 Apartment.configure do |config|
   # These models will not be multi-tenanted, but remain in the global (public) namespace
-  config.excluded_models = ["Client", "Kong::User", "IssueType"]
+  config.excluded_models = ["Client", "Kong::User", "IssueType", "Role"]
 
   # Dynamically get database names to migrate
   config.database_names = lambda { Client.select(:account_name).map(&:account_name) }
